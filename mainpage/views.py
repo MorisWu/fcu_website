c2e5d8e1154bbe842d13da07fb8da448ca118e1f
Lab_Pycharm <wumoris@gmail.com>
2023-09-25 12:57:57 +0800

    mysql catch table test 1.1.5 fix object->objects

--- mainpage/views.py
+++ mainpage/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from database_model.models import citrix_log
 
 def mainpage(request):
-    data_list = list(citrix_log.object.all())
+    data_list = list(citrix_log.objects.all())
     return render(request, 'mainpage/index.html', {'data_list':data_list})
