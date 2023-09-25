d58fc6939185b9faf6b29a5ab5d1db525a02ed14
Lab_Pycharm <wumoris@gmail.com>
2023-09-25 13:59:22 +0800

    mysql catch table test 1.4.1

--- mainpage/views.py
+++ mainpage/views.py
@@ -3,9 +3,10 @@ from database_model.models import citrix_log
 import pandas as pd
 
 def mainpage(request):
-    data_list = list(citrix_log.objects.all())
+    raw_data = citrix_log.objects.all()
+    data_list = list(raw_data)
 
-    pd_data = pd.DataFrame(data_list)
+    pd_data = pd.DataFrame(raw_data)
     pd_data['application_start_date'] = pd.to_datetime(pd_data['application_start_date'])
 
     return render(request, 'mainpage/index.html', {'data_list':data_list})
