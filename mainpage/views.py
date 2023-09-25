710e2ba1963c0b05726664da414e5315b32438b8
Lab_Pycharm <wumoris@gmail.com>
2023-09-25 13:57:01 +0800

    mysql catch table test 1.4

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from database_model.models import citrix_log
+import pandas as pd
 
 def mainpage(request):
     data_list = list(citrix_log.objects.all())
+
+    pd_data = pd.DataFrame(data_list)
+    pd_data['application_start_date'] = pd.to_datetime(pd_data['application_start_date'])
+
     return render(request, 'mainpage/index.html', {'data_list':data_list})
