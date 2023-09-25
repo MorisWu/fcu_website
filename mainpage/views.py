8652c0fcf70fdd2e89097240ffd89c42cb45e551
Lab_Pycharm <wumoris@gmail.com>
2023-09-25 14:55:41 +0800

    mysql catch table test 1.4.4 fix

--- mainpage/views.py
+++ mainpage/views.py
@@ -5,8 +5,7 @@ import pandas as pd
 def mainpage(request):
     raw_data = citrix_log.objects.all()
     data_list = list(raw_data)
-    t = list(type(raw_data))
+    data_type = list(type(raw_data))
     return render(request,
                   'mainpage/index.html',
-                  {'data_list':data_list,
-                   'type':t})
+                  {'data_list':data_list}, data_type)
