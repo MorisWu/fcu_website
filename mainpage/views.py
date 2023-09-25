36bd6d19a80fe85084b2fdd651e55bc0c9b75a9d
Lab_Pycharm <wumoris@gmail.com>
2023-09-25 16:20:30 +0800

    mysql catch table test 1.5.4 fix

--- mainpage/views.py
+++ mainpage/views.py
@@ -9,13 +9,19 @@ def mainpage(request):
     location_group = pd_data.groupby('location_name')
     application_group = location_group.get_group('3ds Max 2022')
 
-    data_list = []
-
     grouped = application_group.groupby(pd.Grouper(key='application_start_date', freq='D'))
 
+    key_list = []
+    num_list = []
     for i in grouped.groups.keys():
-        data_list.append(str(i)+'123')
+        key_list.append(i)
+
+    for i in grouped.size():
+        num_list.append(i)
 
     return render(request,
                   'mainpage/index.html',
-                  {'data_list':data_list})
+                  {
+                    'key_list':key_list,
+                    'num_list':num_list
+                  })
