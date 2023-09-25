c51ed8b037f5c465f9dc6642e098c3ee9367c30f
Lab_Pycharm <wumoris@gmail.com>
2023-09-25 12:37:40 +0800

    mysql catch table test 1.0

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from database_model.models import citrix_log
 
 def mainpage(request):
+    data_list = list(citrix_log.objects.all().values())
     return render(request, 'mainpage/index.html')
