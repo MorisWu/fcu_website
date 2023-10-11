00924c6a36fcb61096dab0bf44d22352ccf305b2
MorisWu <wumoris@gmal.com>
2023-10-11 23:01:20 +0800

    regist model

--- database_model/admin.py
+++ database_model/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import citrix_log
+from .models import citrix_log, pre_process_date_usage_amount_data, pre_process_online_amount_data, application_authorizations_num
 
 admin.site.register(citrix_log)
+admin.site.register(pre_process_date_usage_amount_data)
+admin.site.register(pre_process_online_amount_data)
+admin.site.register(application_authorizations_num)
