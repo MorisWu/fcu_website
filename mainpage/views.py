fe65ee227f91c431840a7c124cf50586984e1e20
Lab_Pycharm <wumoris@gmail.com>
2024-01-02 17:17:53 +0800

    test

--- mainpage/views.py
+++ mainpage/views.py
@@ -175,12 +175,12 @@ def month_online(request):
         app = request.POST['application']
 
     application_online_data = pre_process_online_amount_data.objects.filter(application_name=app).values(
-        'date__month').annotate(max_usage=Max('amount'))
+        'date').annotate(max_usage=Max('amount'))
     date_list = []
     num_list = []
 
     for i in application_online_data:
-        date_list.append(i['date__month'])
+        date_list.append(i['date'])
         num_list.append(i['max_usage'])
 
     trace = go.Figure(
@@ -236,14 +236,14 @@ def vanse_month_data(request):
     if 'application' in request.POST and request.POST['application'] != '':
         app = request.POST['application']
 
-    application_usage_data = vanse_data.objects.filter(application_name=app).values('date').annotate(
+    application_usage_data = vanse_data.objects.filter(application_name=app).values('date__month').annotate(
         max_usage=Max('amount'))
 
     date_list = []
     num_list = []
 
     for i in application_usage_data:
-        date_list.append(i['date'])
+        date_list.append(i['date__month'])
         num_list.append(i['max_usage'])
 
     trace = go.Figure(
